6ebc360b2b9fd900bec80fb11767b6a41a287084
lola gauchet <lola.gauchet86@gmail.com>
2019-05-02 18:18:54 +0200

    formulaire creation de restaurant & validation & update restaurant entity

--- src/Controller/RestaurantController.php
+++ src/Controller/RestaurantController.php
@@ -4,6 +4,12 @@ namespace App\Controller;
 
 use App\Entity\Restaurant;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\TelType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\HttpFoundation\Request;
+use App\Form\RestaurantType;
 
 class RestaurantController extends AbstractController
 {
@@ -15,6 +21,41 @@ class RestaurantController extends AbstractController
 
         return $this->render('restaurant-detail.html.twig', [
             'restaurant' => $restaurant,
+        ]);
+    }
+
+
+    //form ajout restaurant
+    public function form(Request $request)
+    {
+
+        $form = new Restaurant();
+
+        $formulaire = $this->createForm(RestaurantType::class, $form);
+
+        $formulaire->handleRequest($request);
+
+        if ($formulaire->isSubmitted() && $formulaire->isValid()) {
+            $file = $form->getPhoto();
+            $fileName = md5(uniqid()).'.'.$file->guessExtension();
+            $file->move($this->getParameter('photos_directory'), $fileName);
+            $form->setPhoto($fileName);
+
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($form);
+            $em->flush();
+
+            $this->addFlash(
+                'notice',
+                'Votre restaurant a bien été ajouté'
+            );
+            
+            return $this->redirectToRoute('home');
+
+        }
+       
+        return $this->render('form/new.html.twig', [
+            'form' => $formulaire->createView(),
         ]);
     }
 }
--- src/Migrations/Version20190502092932.php
+++ src/Migrations/Version20190502092932.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20190425155445 extends AbstractMigration
+final class Version20190502092932 extends AbstractMigration
 {
     public function getDescription() : string
     {
@@ -29,7 +29,7 @@ final class Version20190425155445 extends AbstractMigration
         $this->addSql('CREATE TABLE menu (id INT AUTO_INCREMENT NOT NULL, carte_id INT DEFAULT NULL, prix INT NOT NULL, description VARCHAR(255) NOT NULL, INDEX IDX_7D053A93C9C7CEB6 (carte_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
         $this->addSql('CREATE TABLE photo (id INT AUTO_INCREMENT NOT NULL, restaurant_id INT DEFAULT NULL, nom VARCHAR(255) NOT NULL, url VARCHAR(255) NOT NULL, INDEX IDX_14B78418B1E7706E (restaurant_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
         $this->addSql('CREATE TABLE plat (id INT AUTO_INCREMENT NOT NULL, carte_id INT NOT NULL, nom VARCHAR(255) NOT NULL, description VARCHAR(255) NOT NULL, INDEX IDX_2038A207C9C7CEB6 (carte_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
-        $this->addSql('CREATE TABLE restaurant (id INT AUTO_INCREMENT NOT NULL, nom VARCHAR(255) NOT NULL, adresse VARCHAR(255) NOT NULL, code_postal VARCHAR(5) NOT NULL, ville VARCHAR(255) NOT NULL, site_web VARCHAR(255) NOT NULL, horaires VARCHAR(255) NOT NULL, type VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE restaurant (id INT AUTO_INCREMENT NOT NULL, nom VARCHAR(255) NOT NULL, adresse VARCHAR(255) NOT NULL, code_postal VARCHAR(5) NOT NULL, ville VARCHAR(255) NOT NULL, telephone VARCHAR(10) NOT NULL, email VARCHAR(255) NOT NULL, site_web VARCHAR(255) NOT NULL, horaires VARCHAR(255) NOT NULL, type VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
         $this->addSql('ALTER TABLE boisson ADD CONSTRAINT FK_8B97C84DC9C7CEB6 FOREIGN KEY (carte_id) REFERENCES carte (id)');
         $this->addSql('ALTER TABLE carte ADD CONSTRAINT FK_BAD4FFFDB1E7706E FOREIGN KEY (restaurant_id) REFERENCES restaurant (id)');
         $this->addSql('ALTER TABLE dessert ADD CONSTRAINT FK_79291B96C9C7CEB6 FOREIGN KEY (carte_id) REFERENCES carte (id)');
