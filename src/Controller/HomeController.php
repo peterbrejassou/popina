1ecb0c5ad9823885ea3de949d54779fe9a8aecdb
Peter Brejassou <peter.brejassou@agenceseize.fr>
2019-05-09 11:15:24 +0200

    Modification homepage

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\Restaurant;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class HomeController extends AbstractController
@@ -13,6 +14,9 @@ class HomeController extends AbstractController
 
     public function home()
     {
-        return $this->render('home.html.twig');
+        $restaurants = $this->getDoctrine()->getRepository(Restaurant::class)->findAll();
+        return $this->render('home.html.twig', [
+            'restaurants' => $restaurants
+        ]);
     }
 }
