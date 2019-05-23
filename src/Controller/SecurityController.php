fd9c5d5155774142743902d7074555ae7b5bce4b
lola gauchet <lola.gauchet86@gmail.com>
2019-05-23 13:30:41 +0200

    log utilisateur

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -8,12 +8,12 @@ use Doctrine\Common\Persistence\ObjectManager;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
-
+use Symfony\Bridge\Monolog\Logger;
 class SecurityController extends AbstractController
 {
-    public function register(Request $request, ObjectManager $manager, UserPasswordEncoderInterface $encoder)
+    public function register(Request $request, ObjectManager $manager, UserPasswordEncoderInterface $encoder, Logger $utilisateurLog)
     {
-        $user = new User();
+        $user = new User(null, null, null, null);
         $form = $this->createForm(RegistrationType::class, $user);
         $form->handleRequest($request);
 
@@ -22,7 +22,8 @@ class SecurityController extends AbstractController
             $user->setPassword($encryptPassword);
             $manager->persist($user);
             $manager->flush();
-
+            $utilisateurLog->info('un nouvel utilisateur a été ajouté',['nom' => $user->getUsername()]);            
+           
             return $this->redirectToRoute('login');
         }
 
@@ -32,7 +33,7 @@ class SecurityController extends AbstractController
     }
 
     public function login()
-    {
+    {   
         return $this->render('front/security/login.html.twig');
     }
 
